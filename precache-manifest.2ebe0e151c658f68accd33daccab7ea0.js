296e1d41176dbe9a0e001cdae0ca6fa268cb96df
yashverma2110 <yash.silver2110@gmail.com>
2019-12-26 18:03:18 +0530

    Updates

---
+++ precache-manifest.2ebe0e151c658f68accd33daccab7ea0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa10b4e91d5360c030a84e223477f371",
+    "url": "/testWebsite/index.html"
+  },
+  {
+    "revision": "27f53564132c48bf6990",
+    "url": "/testWebsite/static/css/main.e3016dc7.chunk.css"
+  },
+  {
+    "revision": "5e531ce76f37fd582e38",
+    "url": "/testWebsite/static/js/2.3ced33de.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/testWebsite/static/js/2.3ced33de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27f53564132c48bf6990",
+    "url": "/testWebsite/static/js/main.e737d4c9.chunk.js"
+  },
+  {
+    "revision": "fd529b876195f882696e",
+    "url": "/testWebsite/static/js/runtime-main.e13f229c.js"
+  }
+]);
